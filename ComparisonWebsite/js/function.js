2a451bfa63829aa207bb9a5817581217f77ff449
LukeDujmic <57417216+LukeDujmic@users.noreply.github.com>
2020-05-14 18:55:31 -0700

    added a buncha files
    stuff i already made, but wasnt in github.

---
+++ ComparisonWebsite/js/function.js
@@ -0,0 +1,39 @@
+function navHoverIn1(event)
+{
+    document.getElementById("link1").style = "opacity: 1; font-weight: 700; box-shadow: 0px 0px 25px rgb(40, 107, 96); transform: scale(1.1);text-shadow: 0px 0px 25px rgba(36, 97, 105, 1);transition: 0.25s;"
+}
+
+function navHoverOut1(event)
+{
+    document.getElementById("link1").style = "opacity: 1; font-weight: 700; box-shadow: 0px 0px 15px rgb(22, 53, 48); transform: scale(1);text-shadow: 0px 0px 0px rgba(22, 53, 48, 0);transition: 0.33s;"
+}
+
+function navHoverIn2(event)
+{
+    document.getElementById("link2").style = "opacity: 0.8; font-weight: 700; box-shadow: 0px 0px 25px rgb(40, 107, 96); transform: scale(1.1);text-shadow: 0px 0px 25px rgba(36, 97, 105, 1);transition: 0.25s;"
+}
+
+function navHoverOut2(event)
+{
+    document.getElementById("link2").style = "opacity: 0.75; font-weight: 100; box-shadow: 0px 0px 15px rgb(22, 53, 48); transform: scale(1);text-shadow: 0px 0px 0px rgba(22, 53, 48, 0);transition: 0.33s;"
+}
+
+function navHoverIn3(event)
+{
+    document.getElementById("link3").style = "opacity: 0.8; font-weight: 700; box-shadow: 0px 0px 25px rgb(40, 107, 96); transform: scale(1.1);text-shadow: 0px 0px 25px rgba(36, 97, 105, 1);transition: 0.25s;"
+}
+
+function navHoverOut3(event)
+{
+    document.getElementById("link3").style = "opacity: 0.75; font-weight: 100; box-shadow: 0px 0px 15px rgb(22, 53, 48); transform: scale(1);text-shadow: 0px 0px 0px rgba(22, 53, 48, 0);transition: 0.33s;"
+}
+
+function navHoverIn4(event)
+{
+    document.getElementById("link4").style = "opacity: 1; font-weight: 700; box-shadow: 0px 0px 25px rgb(40, 107, 96); transform: scale(1.1);text-shadow: 0px 0px 25px rgba(36, 97, 105, 1);transition: 0.25s;"
+}
+
+function navHoverOut4(event)
+{
+    document.getElementById("link4").style = "opacity: 1; font-weight: 700; box-shadow: 0px 0px 15px rgb(22, 53, 48); transform: scale(1);text-shadow: 0px 0px 0px rgba(22, 53, 48, 0);transition: 0.33s;"
+}
